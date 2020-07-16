3448f894c1c7a4151139c453a98451d73bcb0215
zty200329 <1412306160@qq.com>
2020-07-16 21:24:27 +0800

    初始代码
    Signed-off-by: zty200329 <1412306160@qq.com>

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author zty200329
+ * @date 2020/7/16 16:58
+ * @describe:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
