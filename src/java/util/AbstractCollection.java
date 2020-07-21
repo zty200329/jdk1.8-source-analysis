32a11555da830dde0ca3560c5407dfef27f4155f
zty200329 <1412306160@qq.com>
2020-07-21 15:04:42 +0800

    StringBuilder源码阅读
    Signed-off-by: zty200329 <1412306160@qq.com>

--- src/java/util/AbstractCollection.java
+++ src/java/util/AbstractCollection.java
@@ -58,6 +58,10 @@ package java.util;
  * @since 1.2
  */
 
+/**
+ * 从关键字abstract可以看出这个类是一个抽象类，这个类只是实现了一部分的接口，还遗留了一部分接口需要我们在子类中去实现。
+ * @param <E>
+ */
 public abstract class AbstractCollection<E> implements Collection<E> {
     /**
      * Sole constructor.  (For invocation by subclass constructors, typically
@@ -81,6 +85,10 @@ public abstract class AbstractCollection<E> implements Collection<E> {
      * {@inheritDoc}
      *
      * <p>This implementation returns <tt>size() == 0</tt>.
+     */
+    /**
+     * isEmpty()方法通过size()接口是否等于0来判断，而size()接口的实现交给了更为具体的集合类去实现。
+     * @return
      */
     public boolean isEmpty() {
         return size() == 0;
@@ -131,15 +139,22 @@ public abstract class AbstractCollection<E> implements Collection<E> {
      * return list.toArray();
      * }</pre>
      */
+    /**
+     * 分配了一个等大空间的数组，然后依次对数组元素进行赋值
+     * @return
+     */
     public Object[] toArray() {
+        //新建等大的数组
         // Estimate size of array; be prepared to see more or fewer elements
         Object[] r = new Object[size()];
         Iterator<E> it = iterator();
         for (int i = 0; i < r.length; i++) {
+            //判断是否遍历结束，以防多线程操作的时候集合变得更小
             if (! it.hasNext()) // fewer elements than expected
                 return Arrays.copyOf(r, i);
             r[i] = it.next();
         }
+        //判断是否遍历未结束，以防多线程操作的时候集合变得更大，进行扩容
         return it.hasNext() ? finishToArray(r, it) : r;
     }
 
@@ -170,20 +185,29 @@ public abstract class AbstractCollection<E> implements Collection<E> {
      * @throws ArrayStoreException  {@inheritDoc}
      * @throws NullPointerException {@inheritDoc}
      */
+    /**
+     * 泛型方法的`toArray(T[] a)`方法在处理里，会先判断参数数组的大小，
+     * 如果空间足够就使用参数作为元素存储，如果不够则新分配一个。
+     * 在循环中的判断也是一样，如果参数a能够存储则返回a,如果不能再新分配。
+     */
     @SuppressWarnings("unchecked")
     public <T> T[] toArray(T[] a) {
         // Estimate size of array; be prepared to see more or fewer elements
         int size = size();
+        //当数组a的长度大于等于a，直接将a赋予给r，否则使用反射API获取一个长度为size的数组
         T[] r = a.length >= size ? a :
                   (T[])java.lang.reflect.Array
                   .newInstance(a.getClass().getComponentType(), size);
         Iterator<E> it = iterator();
 
         for (int i = 0; i < r.length; i++) {
+            //判断是否遍历结束
             if (! it.hasNext()) { // fewer elements than expected
+                //如果 a == r，将r的每项值赋空，并将a返回
                 if (a == r) {
                     r[i] = null; // null-terminate
                 } else if (a.length < i) {
+                    //如果a的长度小于r，直接调用Arrays.copyOf进行复制获取一个新的数组
                     return Arrays.copyOf(r, i);
                 } else {
                     System.arraycopy(r, 0, a, 0, i);
@@ -193,9 +217,11 @@ public abstract class AbstractCollection<E> implements Collection<E> {
                 }
                 return a;
             }
+            //如果遍历结束，将迭代器获取的值赋给r
             r[i] = (T)it.next();
         }
         // more elements than expected
+        //判断是否遍历未结束，以防多线程操作的时候集合变得更大，进行扩容
         return it.hasNext() ? finishToArray(r, it) : r;
     }
 
@@ -204,6 +230,9 @@ public abstract class AbstractCollection<E> implements Collection<E> {
      * Some VMs reserve some header words in an array.
      * Attempts to allocate larger arrays may result in
      * OutOfMemoryError: Requested array size exceeds VM limit
+     */
+    /**
+     * 数组作为一个对象，需要一定的内存存储对象头信息，对象头信息最大占用内存不可超过8 byte。
      */
     private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
 
@@ -218,6 +247,12 @@ public abstract class AbstractCollection<E> implements Collection<E> {
      *         further elements returned by the iterator, trimmed to size
      */
     @SuppressWarnings("unchecked")
+    /**
+     * finishToArray(T[] r, Iterator<?> it)方法用于数组扩容，
+     * 当数组索引指向最后一个元素+1时，对数组进行扩容：即创建一个大小为（cap + cap/2 +1）的数组，
+     * 然后将原数组的内容复制到新数组中。扩容前需要先判断是否数组长度是否溢出。这里的迭代器是从上层的方法（toArray(T[] t)）传过来的，
+     * 并且这个迭代器已执行了一部分，而不是从头开始迭代的
+     */
     private static <T> T[] finishToArray(T[] r, Iterator<?> it) {
         int i = r.length;
         while (it.hasNext()) {
@@ -235,6 +270,12 @@ public abstract class AbstractCollection<E> implements Collection<E> {
         return (i == r.length) ? r : Arrays.copyOf(r, i);
     }
 
+    /**
+     * hugeCapacity(int minCapacity)方法用来判断该容器是否已经超过了该集合类默认的最大值即（Integer.MAX_VALUE -8），
+     * 一般我们用到这个方法的时候比较少，后面我们会在ArrayList类的学习中，看到ArrayList动态扩容用到了这个方法。
+     * @param minCapacity
+     * @return
+     */
     private static int hugeCapacity(int minCapacity) {
         if (minCapacity < 0) // overflow
             throw new OutOfMemoryError
@@ -258,6 +299,15 @@ public abstract class AbstractCollection<E> implements Collection<E> {
      * @throws IllegalArgumentException      {@inheritDoc}
      * @throws IllegalStateException         {@inheritDoc}
      */
+    /**
+     * 这里的add(E)方法默认抛出了一个异常，为什么这样去定义呢？而不是直接定义为抽象方法？
+     * 如果我们想修改一个不可变的集合时，抛出 UnsupportedOperationException 是正常的行为，
+     * 比如当你用 Collections.unmodifiableXXX() 方法对某个集合进行处理后，再调用这个集合的修改方法（add,remove,set…），
+     * 都会报这个错。因此 AbstractCollection.add(E) 抛出这个错误是准从标准。
+     * 而之所以没有定义为抽象方法，是因为可能有很多地方用不到这个方法，用不到还必须实现，这岂不是让人很困惑么。
+     * @param e
+     * @return
+     */
     public boolean add(E e) {
         throw new UnsupportedOperationException();
     }
@@ -277,6 +327,12 @@ public abstract class AbstractCollection<E> implements Collection<E> {
      * @throws UnsupportedOperationException {@inheritDoc}
      * @throws ClassCastException            {@inheritDoc}
      * @throws NullPointerException          {@inheritDoc}
+     */
+    /**
+     * contains()，remove()，containsAll()，addAll()，removeAll()，retainAll()，clear()，toString()这些方法我们通过阅读源码可以知道，
+     * 都是通过迭代器来实现的，其中有一些方法使用了增强for循环，但其实编译器会将增强for循环编译为使用迭代器的遍历操作。
+     * @param o
+     * @return
      */
     public boolean remove(Object o) {
         Iterator<E> it = iterator();
